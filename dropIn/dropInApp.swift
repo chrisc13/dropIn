5af445e8bb24e8a1cba7fa7df3e277849273e1dd
chris <chrisc03774@gmail.com>
2022-09-17 00:23:40 -0700

    Initial Commit

---
+++ dropIn/dropInApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropInApp.swift
+//  dropIn
+//
+//  Created by Chris Carbajal on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct dropInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
