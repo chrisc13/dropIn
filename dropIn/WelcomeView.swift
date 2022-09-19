e34481a9025d3298c62d6c878a61082c8c3c1f0e
chris <chrisc03774@gmail.com>
2022-09-18 23:59:02 -0700

    AB#1 added new WelcomeView with UI elements, so far tested iPhone 13, 13 mini, 8 for sizing and all look good

---
+++ dropIn/WelcomeView.swift
@@ -0,0 +1,73 @@
+//
+//  WelcomeView.swift
+//  dropIn
+//
+//  Created by Chris Carbajal on 9/18/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    let welcomeMessage = "Welcome, interested in running a pickup game or joining a fun activity around you? Well, just drop in"
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            VStack(spacing: 0) {
+                
+                //top view
+                VStack() {
+                  Text("DropIn")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .padding([.leading, .bottom], 5.0)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                        
+                }
+                .frame(width: geo.size.width, height: geo.size.height * 0.7)
+                .background(Color(UIColor(red:0.22, green:0.24, blue:0.25, alpha:1.0)))
+                
+                //bottom view
+                VStack(alignment: .center, spacing: 0.0) {
+                    Text(welcomeMessage)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10.0)
+                        .frame(maxWidth: .infinity, alignment: .top)
+                        
+                    Spacer()
+                            .frame(minHeight: 0, maxHeight: 30)
+                    HStack(){
+                        Button("Login") {
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 44)
+                        .foregroundColor(Color.black)
+                        Button("Sign Up") {
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity,  minHeight: 44)
+                        .foregroundColor(Color.white)
+                        .background(Color(UIColor(red:0.15, green:0.20, blue:0.22, alpha:1.0)))
+                    }
+                    .background(Color(UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.0)))
+                    .cornerRadius(35)
+                    .padding(20)
+                    
+                }
+                
+                .frame(width: geo.size.width, height: geo.size.height * 0.3)
+                .background(Color(UIColor(red:0.83, green:0.82, blue:0.80, alpha:1.0)))
+                
+
+          }
+        }
+        .ignoresSafeArea()
+        
+       
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
