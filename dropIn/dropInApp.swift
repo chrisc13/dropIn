4345843aadfcebb13257758383174d926bd66849
chris <chrisc03774@gmail.com>
2022-09-21 13:06:07 -0700

    US#2 added firebase dependency with plist and allowed keychain sharing

--- dropIn/dropInApp.swift
+++ dropIn/dropInApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct dropInApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
