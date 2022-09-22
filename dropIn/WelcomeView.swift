eedac2ec1154f336532de40107ac9efd7cf91035
chris <chrisc03774@gmail.com>
2022-09-21 20:06:06 -0700

    US#3 added segue package to switch views, login view created and fully working (does segue to Home View ) if user is valid, and saves it to user defaults

--- dropIn/WelcomeView.swift
+++ dropIn/WelcomeView.swift
@@ -6,63 +6,75 @@
 //
 
 import SwiftUI
+import SwiftUISegues
 
 struct WelcomeView: View {
     let welcomeMessage = "Welcome, interested in running a pickup game or joining a fun activity around you? Well, just drop in"
-    
+    enum Route: Hashable {
+           case segueToLoginView
+       }
+    @State private var route: Route? = nil
     var body: some View {
         
-        GeometryReader { geo in
-            VStack(spacing: 0) {
-                
-                //top view
-                VStack() {
-                  Text("DropIn")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.white)
-                    .padding([.leading, .bottom], 5.0)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        
+        NavigationView{
+            GeometryReader { geo in
+                VStack(spacing: 0) {
+                    
+                    //top view
+                    VStack() {
+                        Text("DropIn")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                            .padding([.leading, .bottom], 5.0)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
                         
-                }
-                .frame(width: geo.size.width, height: geo.size.height * 0.7)
-                .background(Color(UIColor(red:0.22, green:0.24, blue:0.25, alpha:1.0)))
-                
-                //bottom view
-                VStack(alignment: .center, spacing: 0.0) {
-                    Text(welcomeMessage)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 10.0)
-                        .frame(maxWidth: .infinity, alignment: .top)
-                        
-                    Spacer()
-                            .frame(minHeight: 0, maxHeight: 30)
-                    HStack(){
-                        Button("Login") {
-                        }
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 44)
-                        .foregroundColor(Color.black)
-                        Button("Sign Up") {
-                        }
-                        .frame(minWidth: 0, maxWidth: .infinity,  minHeight: 44)
-                        .foregroundColor(Color.white)
-                        .background(Color(UIColor(red:0.15, green:0.20, blue:0.22, alpha:1.0)))
                     }
-                    .background(Color(UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.0)))
-                    .cornerRadius(35)
-                    .padding(20)
+                    .frame(width: geo.size.width, height: geo.size.height * 0.7)
+                    .background(Color(UIColor(red:0.22, green:0.24, blue:0.25, alpha:1.0)))
+                    
+                    //bottom view
+                    VStack(alignment: .center, spacing: 0.0) {
+                        Text(welcomeMessage)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 10.0)
+                            .frame(maxWidth: .infinity, alignment: .top)
+                        
+                        Spacer()
+                            .frame(minHeight: 0, maxHeight: 30)
+                        HStack(){
+                            Button("Login") {
+                                route = .segueToLoginView
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 44)
+                            .foregroundColor(Color.black)
+                            Button("Sign Up") {
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity,  minHeight: 44)
+                            .foregroundColor(Color.white)
+                            .background(Color(UIColor(red:0.15, green:0.20, blue:0.22, alpha:1.0)))
+                        }
+                        .background(Color(UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.0)))
+                        .cornerRadius(35)
+                        .padding(20)
+                        
+                    }
+                    
+                    .frame(width: geo.size.width, height: geo.size.height * 0.3)
+                    .background(Color(UIColor(red:0.83, green:0.82, blue:0.80, alpha:1.0)))
+                    
                     
                 }
-                
-                .frame(width: geo.size.width, height: geo.size.height * 0.3)
-                .background(Color(UIColor(red:0.83, green:0.82, blue:0.80, alpha:1.0)))
-                
-
-          }
+            }
+            .ignoresSafeArea()
+            
+            // Here are individual, mixed segues, with their destinations
+            .segue(.push, tag: .segueToLoginView, selection: $route) {
+                LoginView()
+            }
         }
-        .ignoresSafeArea()
-        
-       
+        .accentColor(.white)
     }
 }
 
